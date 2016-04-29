051337cc96920a2faef3b607c15fd092c0b490cf
edoume <iphone.courriel@gmail.com>
2016-04-29 15:51:59 +0200

    room

--- room/js/serverless-webrtc.js
+++ room/js/serverless-webrtc.js
@@ -1,148 +1,315 @@
-function WebRTCChat(cfg, con, sendTyping) {
-    var self = this;
+/* See also:
+    http://www.html5rocks.com/en/tutorials/webrtc/basics/
+    https://code.google.com/p/webrtc-samples/source/browse/trunk/apprtc/index.html
 
-/* WebRTC setup for broser-to-browser connection */
-    self.cfg = {'iceServers':[{"url":"stun:23.21.150.121"}]}; //{"url":"stun:23.21.150.121"}
-    self.con = {'optional':  [{'DtlsSrtpKeyAgreement': true}] };
-    self.activeChannel;
-    self.activeConnection;
-    self.roomName;
+    https://webrtc-demos.appspot.com/html/pc1.html
+*/
 
-    self.readystate = false;
-    self.sendTyping = true;
+var cfg = {'iceServers': [{'url': 'stun:23.21.150.121'}]},
+  con = { 'optional': [{'DtlsSrtpKeyAgreement': true}] }
 
-/* WebRTC + PGP CHAT CONNECTION CODE */
+/* THIS IS ALICE, THE CALLER/SENDER */
 
-    /* THE HOST (initiates the chat) */
+var pc1 = new RTCPeerConnection(cfg, con),
+  dc1 = null, tn1 = null
 
-    self.hostChat = function(offer_callback, ready_callback) {
-        var hostConnection = new RTCPeerConnection(self.cfg, self.con);                                 // init connection
-        self.initConnection(hostConnection, offer_callback);
+// Since the same JS file contains code for both sides of the connection,
+// activedc tracks which of the two possible datachannel variables we're using.
+var activedc
 
-        var hostChannel = hostConnection.createDataChannel('chat', {reliable:true, ordered:true});      // init channel
-        self.initChannel(hostChannel, ready_callback);
-        
-        console.log("Creating RTC Chat Host Offer...");
-        hostConnection.createOffer(self.handleDescription, self.handleDescriptionFailure);
-        // copy paste this offer to all clients who want to join
-        // they paste their answer back, which goes into handleAnswerFromClient
+var pc1icedone = false
+
+var sdpConstraints = {
+  optional: [],
+  mandatory: {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true
+  }
+}
+
+$('#showLocalOffer').modal('hide')
+$('#getRemoteAnswer').modal('hide')
+$('#waitForConnection').modal('hide')
+$('#createOrJoin').modal('show')
+
+$('#createBtn').click(function () {
+  $('#showLocalOffer').modal('show')
+  createLocalOffer()
+})
+
+$('#joinBtn').click(function () {
+  navigator.getUserMedia = navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia
+  navigator.getUserMedia({video: true, audio: true}, function (stream) {
+    var video = document.getElementById('localVideo')
+    video.src = window.URL.createObjectURL(stream)
+    video.play()
+    pc2.addStream(stream)
+  }, function (error) {
+    console.log('Error adding stream to pc2: ' + error)
+  })
+  $('#getRemoteOffer').modal('show')
+})
+
+$('#offerSentBtn').click(function () {
+  $('#getRemoteAnswer').modal('show')
+})
+
+$('#offerRecdBtn').click(function () {
+  var offer = $('#remoteOffer').val()
+  var offerDesc = new RTCSessionDescription(JSON.parse(offer))
+  console.log('Received remote offer', offerDesc)
+  writeToChatLog('Received remote offer', 'text-success')
+  handleOfferFromPC1(offerDesc)
+  $('#showLocalAnswer').modal('show')
+})
+
+$('#answerSentBtn').click(function () {
+  $('#waitForConnection').modal('show')
+})
+
+$('#answerRecdBtn').click(function () {
+  var answer = $('#remoteAnswer').val()
+  var answerDesc = new RTCSessionDescription(JSON.parse(answer))
+  handleAnswerFromPC2(answerDesc)
+  $('#waitForConnection').modal('show')
+})
+
+$('#fileBtn').change(function () {
+  var file = this.files[0]
+  console.log(file)
+
+  sendFile(file)
+})
+
+function fileSent (file) {
+  console.log(file + ' sent')
+}
+
+function fileProgress (file) {
+  console.log(file + ' progress')
+}
+
+function sendFile (data) {
+  if (data.size) {
+    FileSender.send({
+      file: data,
+      onFileSent: fileSent,
+      onFileProgress: fileProgress,
+    })
+  }
+}
+
+function sendMessage () {
+  if ($('#messageTextBox').val()) {
+    var channel = new RTCMultiSession()
+    writeToChatLog($('#messageTextBox').val(), 'text-success')
+    channel.send({message: $('#messageTextBox').val()})
+    $('#messageTextBox').val('')
+
+    // Scroll chat text area to the bottom on new input.
+    $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight)
+  }
+
+  return false
+}
+
+function setupDC1 () {
+  try {
+    var fileReceiver1 = new FileReceiver()
+    dc1 = pc1.createDataChannel('test', {reliable: true})
+    activedc = dc1
+    console.log('Created datachannel (pc1)')
+    dc1.onopen = function (e) {
+      console.log('data channel connect')
+      $('#waitForConnection').modal('hide')
+      $('#waitForConnection').remove()
     }
-
-    self.handleAnswerFromClient = function(answer) {
-        var answerDesc = new RTCSessionDescription(answer.rtc);
-        console.log("Received Chat RTC Join Answer: ", answerDesc);
-        self.activeConnection.setRemoteDescription(answerDesc);
-
-        writeToChatLog("Started hosting a chat.", "text-success alert-success");
-
-        // hostChannel.onopen will trigger once the connection is complete (enabling the chat window)
-    }
-
-    /* THE JOINEE (joins an existing chat) */
-
-    self.joinChat = function(offer, answer_callback, ready_callback) {
-        var clientConnection = new RTCPeerConnection(cfg, con);
-        self.initConnection(clientConnection, answer_callback);
-
-        clientConnection.ondatachannel = function (e) {                                     // once client receives a good data channel from the host
-            // Chrome sends event, FF sends raw channel
-            var clientChannel = e.channel || e;
-            self.initChannel(clientChannel, ready_callback);
-            writeToChatLog("Joined a chat.", "text-success alert-success");
-            // clientChannel.onopen will then trigger once the connection is complete (enabling the chat window)
-        };
-
-        self.roomName = offer.roomName;
-
-        var offerDesc = new RTCSessionDescription(offer.rtc);
-        console.log("Received Chat RTC Host Offer: ", offerDesc);
-        self.activeConnection.setRemoteDescription(offerDesc);
-        
-        console.log("Answering Chat Host Offer...");
-        self.activeConnection.createAnswer(self.handleDescription, self.handleDescriptionFailure);
-
-        // ondatachannel triggers once the client has accepted our answer ^
-    }
-
-    self.initConnection = function(conn, callback) {
-        self.activeConnection = conn;
-        // these aren't really necessary
-        conn.addStream(window.myStream);
-        conn.onconnection                   = function (state) {console.info('Chat connection complete: ', event);}
-        conn.onsignalingstatechange         = function (state) {console.info('Signaling state change: ', state); if (self.activeConnection.iceConnectionState == "disconnected") self.writeToChatLog("Chat partner disconnected.", "text-warning alert-error");}
-        conn.oniceconnectionstatechange     = function (state) {console.info('Signaling ICE connection state change: ', state); if (self.activeConnection.iceConnectionState == "disconnected") self.writeToChatLog("Chat partner disconnected.", "text-warning alert-error");}
-        conn.onicegatheringstatechange      = function (state) {console.info('Signaling ICE setup state change: ', state);}
-        conn.onaddstream = function (event) {
-            var video = document.getElementById('theirVideo');
-            if (window.URL) video.src = window.URL.createObjectURL(event.stream);
-            else video.src = event.stream;
-            console.log("YAAAAAAAAgotvideo");
+    dc1.onmessage = function (e) {
+      console.log('Got message (pc1)', e.data)
+      if (e.data.size) {
+        fileReceiver1.receive(e.data, {})
+      } else {
+        if (e.data.charCodeAt(0) == 2) {
+          // The first message we get from Firefox (but not Chrome)
+          // is literal ASCII 2 and I don't understand why -- if we
+          // leave it in, JSON.parse() will barf.
+          return
         }
-        //this is the important one
-        conn.onicecandidate = function (event) {
-            // when browser has determined how to form a connection, generate offer or answer with ICE connection details and PGP public key
-            if (event.candidate == null) {
-                console.log("Valid ICE connection candidate determined.");
-                var offer_or_answer = JSON.stringify({
-                    rtc: self.activeConnection.localDescription,
-                    roomName: self.roomName
-                });
-                // pass the offer or answer to the callback for display to the user or to send over some other communication channel
-                if (callback) callback(offer_or_answer);
-            }
-        };
-        conn.onfailure = function(details) {callback(details)};
-        console.log("Initialized Connection: ", conn);
-    }
-
-    self.initChannel = function(chan, callback) {
-        self.activeChannel = chan;
-        // once the channel is open, trigger the callback to enable the chat window or carry out other logic
-        chan.onopen = function (e) { console.log('Data Channel Connected.'); self.readystate = true; if (callback) callback(e);}
-        chan.onmessage = self.receiveMessage;
-        console.log("Initialized Data Channel: ", chan);
-    }
-
-    self.handleDescription = function(desc) {
-        self.activeConnection.setLocalDescription(desc, function () {});
-    }
-
-    self.handleDescriptionFailure = function() {
-        console.warn("Failed to create or answer chat offer.");
-        self.activeConnection.onfailure("Invalid or expired chat offer. Please try again.")
-    }
-
-    // messaging functions
-
-    self.sendTypingMessage = function() {
-        self.activeChannel.send(JSON.stringify({message:null,typing:true}));
-    }
-
-    self.sendMessage = function(message) {
-        self.activeChannel.send(JSON.stringify({message: message}));
-        self.writeToChatLog(message, "text-success sent insecure", false);
-    }
-
-    self.receiveMessage = function(event) {
-        var data = JSON.parse(event.data);
-        if (data.type === 'file' || event.data.size) console.log("Receiving a file.");
-        else {
-            if (data.typing && !data.message) {
-                console.log("Partner is typing...");
-                self.displayPartnerTyping();
-            }
-            else {
-                console.log("Received a message: ", data.message);
-                self.writeToChatLog(data.message, "text-info recv", false);
-            }
+        console.log(e)
+        var data = JSON.parse(e.data)
+        if (data.type === 'file') {
+          fileReceiver1.receive(e.data, {})
+        } else {
+          writeToChatLog(data.message, 'text-info')
+          // Scroll chat text area to the bottom on new input.
+          $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight)
         }
+      }
     }
+  } catch (e) { console.warn('No data channel (pc1)', e); }
+}
 
-/* Utilities */
+function createLocalOffer () {
+  console.log('video1')
+  navigator.getUserMedia = navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia
+  navigator.getUserMedia({video: true, audio: true}, function (stream) {
+    var video = document.getElementById('localVideo')
+    video.src = window.URL.createObjectURL(stream)
+    video.play()
+    pc1.addStream(stream)
+    console.log(stream)
+    console.log('adding stream to pc1')
+    setupDC1()
+    pc1.createOffer(function (desc) {
+      pc1.setLocalDescription(desc, function () {}, function () {})
+      console.log('created local offer', desc)
+    },
+    function () { console.warn("Couldn't create offer") },
+    sdpConstraints)
+  }, function (error) {
+    console.log('Error adding stream to pc1: ' + error)
+  })
+}
 
-    // set these to your own functions using WebRTCChat.writeToChatLog = function(...) {...}
+pc1.onicecandidate = function (e) {
+  console.log('ICE candidate (pc1)', e)
+  if (e.candidate == null) {
+    $('#localOffer').html(JSON.stringify(pc1.localDescription))
+  }
+}
 
-    self.writeToChatLog = function(message, message_type, secure) {console.log("-> ", message, message_type, secure);}
+function handleOnaddstream (e) {
+  console.log('Got remote stream', e.stream)
+  var el = document.getElementById('remoteVideo')
+  el.autoplay = true
+  attachMediaStream(el, e.stream)
+}
 
-    self.displayPartnerTyping = function() {console.log("-> Typing...");}
+pc1.onaddstream = handleOnaddstream
+
+function handleOnconnection () {
+  console.log('Datachannel connected')
+  writeToChatLog('Datachannel connected', 'text-success')
+  $('#waitForConnection').modal('hide')
+  // If we didn't call remove() here, there would be a race on pc2:
+  //   - first onconnection() hides the dialog, then someone clicks
+  //     on answerSentBtn which shows it, and it stays shown forever.
+  $('#waitForConnection').remove()
+  $('#showLocalAnswer').modal('hide')
+  $('#messageTextBox').focus()
+}
+
+pc1.onconnection = handleOnconnection
+
+function onsignalingstatechange (state) {
+  console.info('signaling state change:', state)
+}
+
+function oniceconnectionstatechange (state) {
+  console.info('ice connection state change:', state)
+}
+
+function onicegatheringstatechange (state) {
+  console.info('ice gathering state change:', state)
+}
+
+pc1.onsignalingstatechange = onsignalingstatechange
+pc1.oniceconnectionstatechange = oniceconnectionstatechange
+pc1.onicegatheringstatechange = onicegatheringstatechange
+
+function handleAnswerFromPC2 (answerDesc) {
+  console.log('Received remote answer: ', answerDesc)
+  writeToChatLog('Received remote answer', 'text-success')
+  pc1.setRemoteDescription(answerDesc)
+}
+
+function handleCandidateFromPC2 (iceCandidate) {
+  pc1.addIceCandidate(iceCandidate)
+}
+
+/* THIS IS BOB, THE ANSWERER/RECEIVER */
+
+var pc2 = new RTCPeerConnection(cfg, con),
+  dc2 = null
+
+var pc2icedone = false
+
+pc2.ondatachannel = function (e) {
+  var fileReceiver2 = new FileReceiver()
+  var datachannel = e.channel || e; // Chrome sends event, FF sends raw channel
+  console.log('Received datachannel (pc2)', arguments)
+  dc2 = datachannel
+  activedc = dc2
+  dc2.onopen = function (e) {
+    console.log('data channel connect')
+    $('#waitForConnection').modal('hide')
+    $('#waitForConnection').remove()
+  }
+  dc2.onmessage = function (e) {
+    console.log('Got message (pc2)', e.data)
+    if (e.data.size) {
+      fileReceiver2.receive(e.data, {})
+    } else {
+      var data = JSON.parse(e.data)
+      if (data.type === 'file') {
+        fileReceiver2.receive(e.data, {})
+      } else {
+        writeToChatLog(data.message, 'text-info')
+        // Scroll chat text area to the bottom on new input.
+        $('#chatlog').scrollTop($('#chatlog')[0].scrollHeight)
+      }
+    }
+  }
+}
+
+function handleOfferFromPC1 (offerDesc) {
+  pc2.setRemoteDescription(offerDesc)
+  pc2.createAnswer(function (answerDesc) {
+    writeToChatLog('Created local answer', 'text-success')
+    console.log('Created local answer: ', answerDesc)
+    pc2.setLocalDescription(answerDesc)
+  },
+  function () { console.warn("Couldn't create offer") },
+  sdpConstraints)
+}
+
+pc2.onicecandidate = function (e) {
+  console.log('ICE candidate (pc2)', e)
+  if (e.candidate == null) {
+    $('#localAnswer').html(JSON.stringify(pc2.localDescription))
+  }
+}
+
+pc2.onsignalingstatechange = onsignalingstatechange
+pc2.oniceconnectionstatechange = oniceconnectionstatechange
+pc2.onicegatheringstatechange = onicegatheringstatechange
+
+function handleCandidateFromPC1 (iceCandidate) {
+  pc2.addIceCandidate(iceCandidate)
+}
+
+pc2.onaddstream = handleOnaddstream
+pc2.onconnection = handleOnconnection
+
+function getTimestamp () {
+  var totalSec = new Date().getTime() / 1000
+  var hours = parseInt(totalSec / 3600) % 24
+  var minutes = parseInt(totalSec / 60) % 60
+  var seconds = parseInt(totalSec % 60)
+
+  var result = (hours < 10 ? '0' + hours : hours) + ':' +
+    (minutes < 10 ? '0' + minutes : minutes) + ':' +
+    (seconds < 10 ? '0' + seconds : seconds)
+
+  return result
+}
+
+function writeToChatLog (message, message_type) {
+  document.getElementById('chatlog').innerHTML += '<p class="' + message_type + '">' + '[' + getTimestamp() + '] ' + message + '</p>'
 }
